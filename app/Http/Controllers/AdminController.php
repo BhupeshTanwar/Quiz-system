4be663e37d4754cba730a1e1d0fd53c6fb69a61c
Bhupesh Saini <itsbhupeshtanwar786@gmail.com>
2025-06-18 11:15:05 +0530

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\Models\Admin;
+
+class AdminController extends Controller
+{
+    //
+    function login(Request $req)
+    {
+        $validation = $req->validate([
+            "name" => "required",
+            "password" => "required"
+        ]);
+
+        $admin = Admin::where([
+            ['name', '=', $req->name],
+            ['password', '=', $req->password]
+        ])->first();
+
+        if (!$admin) {
+            $validation = $req->validate(
+                [
+                    "user" => "required",
+                ],
+                [
+                    "user.required" => "User does not exist.",
+                ]
+            );
+        }
+
+        Session::put('admin', $admin);
+        return redirect('dashboard');
+    }
+
+    function dashboard()
+    {
+        $admin = Session::get('admin');
+        if ($admin) {
+            return view('admin', ['name' => $admin->name]);
+        } else {
+            return redirect('admin-login');
+        }
+    }
+}
---
+++ resources/views/admin-login.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Admin Login</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-white-100 flex items-center justify-center min-h-screen">
+    <div class=" bg-white p-8 rounded-2xl shadow-lg w-full max-w-sm">
+        <h2 class="text-2xl text-center text-gray-800 mb-6">Admin Login</h2>
+
+        @error('user')
+        <div class="text-red-500">{{$message}}</div>
+        @enderror
+
+        <form action="/admin-login" method="post" class="space-y-4">
+            @csrf
+            <div>
+                <label for="" class="text-gray-700 mb-1">Admin Name :</label>
+                <input type="text" name="name" placeholder="Enter Your Name"
+                    class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
+                @error('name')
+                <div class="text-red-500">{{$message}}</div>
+                @enderror
+            </div>
+
+            <div>
+                <label for="" class="text-gray-700 mb-1">Password :</label>
+                <input type="password" name="password" placeholder="Enter Admin Password"
+                    class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
+                @error('password')
+                <div class="text-red-500">{{$message}}</div>
+                @enderror
+            </div>
+            <button type="submit" class="w-full px-4 py-2 border bg-blue-400 rounded-xl text-white">Login</button>
+        </form>
+
+    </div>
+</body>
+
+</html>
